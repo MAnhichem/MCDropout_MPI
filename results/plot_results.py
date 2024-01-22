acbe8b9822ebcf930fa860a9b75590ad647c86f5
Mehdi Anhichem <m.anhichem@liverpool.ac.uk>
2024-01-22 17:32:04 +0000

    Update 22012024

--- results/plot_results.py
+++ results/plot_results.py
@@ -49,7 +49,10 @@ prediction_time_n6 = pd.read_csv('./results/prediction_time_study_n6.csv')
 prediction_time_n8 = pd.read_csv('./results/prediction_time_study_n8.csv')
 
 mc_samples = prediction_time_n2['mc_samples'].values
-pred_time_serial = prediction_time_n2['pred_time_serial'].values
+pred_time_serial_n2 = prediction_time_n2['pred_time_serial'].values
+pred_time_serial_n4 = prediction_time_n4['pred_time_serial'].values
+pred_time_serial_n6 = prediction_time_n6['pred_time_serial'].values
+pred_time_serial_n8 = prediction_time_n8['pred_time_serial'].values
 pred_time_n2 = prediction_time_n2['pred_time_parallel'].values
 pred_time_n4 = prediction_time_n4['pred_time_parallel'].values
 pred_time_n6 = prediction_time_n6['pred_time_parallel'].values
@@ -59,11 +62,11 @@ pred_time_n8 = prediction_time_n8['pred_time_parallel'].values
 # PLOT SPEED-UP FIGURE
 #==============================================================================
 fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True)
-plt.plot(mc_samples, pred_time_serial, marker='o')
-plt.plot(mc_samples, pred_time_n2, marker='o')
-plt.plot(mc_samples, pred_time_n4, marker='o')
-plt.plot(mc_samples, pred_time_n6, marker='o')
-plt.plot(mc_samples, pred_time_n8, marker='o')
+plt.plot(mc_samples, pred_time_serial_n2, marker='o', c = 'tab:gray')
+plt.plot(mc_samples, pred_time_n2, marker='o', c = 'tab:blue')
+plt.plot(mc_samples, pred_time_n4, marker='o', c = 'tab:orange')
+plt.plot(mc_samples, pred_time_n6, marker='o', c = 'tab:green')
+plt.plot(mc_samples, pred_time_n8, marker='o', c = 'tab:red')
 plt.xscale('log')
 plt.xlabel(r'Monte Carlo samples [-]', fontsize=23)
 plt.ylabel(r'Prediction time [s]', fontsize=23)
@@ -73,10 +76,10 @@ plt.savefig(working_path + '/MCDropout_MPI/results/prediction_time_study.png')
 
 fig, ax = plt.subplots(figsize=(8,6), constrained_layout=True)
 # plt.plot(mc_samples, pred_time_serial, marker='o')
-plt.plot(mc_samples, pred_time_serial/pred_time_n2, marker='o')
-plt.plot(mc_samples, pred_time_serial/pred_time_n4, marker='o')
-plt.plot(mc_samples, pred_time_serial/pred_time_n6, marker='o')
-plt.plot(mc_samples, pred_time_serial/pred_time_n8, marker='o')
+plt.plot(mc_samples, pred_time_serial_n2/pred_time_n2, marker='o', c = 'tab:blue')
+plt.plot(mc_samples, pred_time_serial_n4/pred_time_n4, marker='o', c = 'tab:orange')
+plt.plot(mc_samples, pred_time_serial_n6/pred_time_n6, marker='o', c = 'tab:green')
+plt.plot(mc_samples, pred_time_serial_n8/pred_time_n8, marker='o', c = 'tab:red')
 plt.xscale('log')
 plt.xlabel(r'Monte Carlo samples [-]', fontsize=23)
 plt.ylabel(r'Speed-up [-]', fontsize=23)
